45a0890647e6d142f1b724878957dc9249855149
Harmony Trevena <harmonytrevena@gmail.com>
2020-10-12 11:30:18 -0400

    Spun up new creat-react-app and added react-router.

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = props => (
+    <>
+        <h1>About Page</h1>
+        <p>Welcome to the about page!</p>
+        <p>This is all about ME!</p>
+    </>
+);
+
+export default About;
